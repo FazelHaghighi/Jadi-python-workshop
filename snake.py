bd595a902f4b116b664ad50b449f75c2bb9b7dc1
FazelHaghighi <fazel.haghighi1399@gmail.com>
2023-05-29 14:53:31 +0330

    refactor: refactor the code for better performance

--- snake.py
+++ snake.py
@@ -1,56 +1,54 @@
-# Example file showing a basic pygame "game loop"
 import pygame
 import random
 
-width = 600
-height = 400
+# Constants
+WIDTH = 600
+HEIGHT = 400
+BLOCK_SIZE = 10
+FPS = 10
+WALL_COLOR = (255, 0, 0)  # Red
+BACKGROUND_COLOR = (128, 0, 128)  # Purple
+SNAKE_COLOR = (255, 255, 255)  # White
+FOOD_COLOR = (0, 255, 0)  # Green
 
-snake = [(width / 2, height / 2)]
-
-# pygame setup
+# Initialize pygame
 pygame.init()
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
+
+# Snake initialization
+snake = [(WIDTH // 2, HEIGHT // 2)]
+direction = ""
 running = True
 
-direction = ""
+def get_food_position():
+    x = random.randrange(BLOCK_SIZE, WIDTH - BLOCK_SIZE, BLOCK_SIZE)
+    y = random.randrange(BLOCK_SIZE, HEIGHT - BLOCK_SIZE, BLOCK_SIZE)
+    return x, y
 
-def food_position():
-    food = [random.randrange(10, (width-10), 10), 
-            random.randrange(10, (height-10), 10)]
-    return food
-
-food = food_position()
+food = get_food_position()
 
 def is_dead(snake):
-    if snake[0][0] < 0 or snake[0][0] > width - 20:
-        return True
-    elif snake[0][1] < 0 or snake[0][1] > height - 20:
-        return True
-    return False
+    x, y = snake[0]
+    return not (0 <= x < WIDTH and 0 <= y < HEIGHT)
 
-def draw_wall():
-    for i in range(0, width, 10):
-        pygame.draw.rect(screen, "red", (i, 0, 10, 10))
-        pygame.draw.rect(screen, "red", (i, height-10, 10, 10))
-    for i in range(0, height, 10):
-        pygame.draw.rect(screen, "red", (0, i, 10, 10))
-        pygame.draw.rect(screen, "red", (width-10, i, 10, 10))
-
+def draw_walls():
+    for i in range(0, WIDTH, BLOCK_SIZE):
+        pygame.draw.rect(screen, WALL_COLOR, (i, 0, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(screen, WALL_COLOR, (i, HEIGHT - BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
+    for i in range(0, HEIGHT, BLOCK_SIZE):
+        pygame.draw.rect(screen, WALL_COLOR, (0, i, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(screen, WALL_COLOR, (WIDTH - BLOCK_SIZE, i, BLOCK_SIZE, BLOCK_SIZE))
 
 def increase_snake(snake):
-    # increase the size of the snake by five blocks
-    for i in range(1):
-        snake.append((snake[-1][0], snake[-1][1]))
-
+    # Increase the size of the snake by one block
+    snake.append(snake[-1])
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] and direction != "d":
         direction = "u"
@@ -60,38 +58,37 @@ while running:
         direction = "r"
     if keys[pygame.K_LEFT] and direction != "r":
         direction = "l"
-        
+
     if is_dead(snake):
         running = False
-    
-    # move the snake
+
+    dx, dy = 0, 0
     if direction == "u":
-        snake.insert(0, (snake[0][0], snake[0][1] - 10))
-    if direction == "d":
-        snake.insert(0, (snake[0][0], snake[0][1] + 10))
-    if direction == "r":
-        snake.insert(0, (snake[0][0] + 10, snake[0][1]))
-    if direction == "l":
-        snake.insert(0, (snake[0][0] - 10, snake[0][1]))
-    
+        dy = -BLOCK_SIZE
+    elif direction == "d":
+        dy = BLOCK_SIZE
+    elif direction == "r":
+        dx = BLOCK_SIZE
+    elif direction == "l":
+        dx = -BLOCK_SIZE
+
+    head = (snake[0][0] + dx, snake[0][1] + dy)
+    snake.insert(0, head)
+
     if len(snake) > 1:
         snake.pop()
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill(BACKGROUND_COLOR)
     for s in snake:
-        pygame.draw.rect(screen, "white", (s[0], s[1], 10, 10))
-    pygame.draw.rect(screen, "green", (food[0], food[1], 10, 10))
-    draw_wall()
+        pygame.draw.rect(screen, SNAKE_COLOR, (s[0], s[1], BLOCK_SIZE, BLOCK_SIZE))
+    pygame.draw.rect(screen, FOOD_COLOR, (food[0], food[1], BLOCK_SIZE, BLOCK_SIZE))
+    draw_walls()
 
-    # print(snake[0][0], snake[0][1], food[0], food[1])
-    if snake[0][0] == food[0] and snake[0][1] == food[1]:
+    if snake[0] == food:
         increase_snake(snake)
-        food = food_position()
-        
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+        food = get_food_position()
 
-    clock.tick(10)  # limits FPS to 10
+    pygame.display.flip()
+    clock.tick(FPS)
 
 pygame.quit()
